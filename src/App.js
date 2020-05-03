1f7cec2045c5eaeb1a692c960ae673323e744920
Viktoria Kovalenko <rambambam72@gmail.com>
2020-05-03 22:07:58 +0300

    learn classes components and rouning

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header.jsx';
+import Navbar from './components/Navbar.jsx';
+import Content from './components/Content.jsx';
+import MainPage from './components/MainPage';
+import InfoPage from './components/Info';
+import Contact from './components/Contact';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+const App = () => {
+return (
+    <BrowserRouter>
+
+    <div className='app-class'>
+      <Header />
+      <Navbar />
+      <div>
+      <Route path='/main' component={MainPage}/>
+      <Route path='/posts' component={Content}/>
+      <Route path='/info' component={InfoPage}/>
+      <Route path='/contact' component={Contact}/>
+      </div>
+   </div>
+
+   </BrowserRouter>
+)
+
 }
 
 export default App;
